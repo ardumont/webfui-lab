9eac40b237f8f76680c00872e741a25a4a25ad92
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-02-26 20:48:14 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to webfui-lab
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/webfui_lab/core_test.clj
@@ -0,0 +1,7 @@
+(ns webfui-lab.core-test
+  (:use clojure.test
+        webfui-lab.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
