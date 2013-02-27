82b7d16508a5e0ce61ec6e35c7684dbb6be31949
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-02-27 18:56:34 +0100

    improve description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject webfui-lab "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "webfui learning playground"
+  :url "http://github.com/ardumont/webfui-lab"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
